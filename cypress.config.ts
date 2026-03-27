1b0b7371b29940efccefba591d227c5679138319
Daniel Garcia <josedanielgarciamoreno@outlook.com>
2026-03-27 14:58:19 +0100

    Añadido archivo de configuración de Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:4321",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    supportFile: "cypress/support/e2e.ts"
+  },
+  video: true,
+  screenshotsFolder: "screenshots",
+  videosFolder: "videos"
+});
